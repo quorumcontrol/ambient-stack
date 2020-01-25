17088b7cf278e0300bba7cacc3e29a98422e36f8
Topper Bowers <topper@toppingdesign.com>
2020-01-25 13:51:02 +0100

    componetize the standup

---
+++ demo/src/components/standupreport.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Box, Heading, Text} from 'grommet';
+
+
+export interface StandupProps {
+    name: string
+    today: string
+    yesterday:string
+    blockers?:string
+    icon: JSX.Element
+}
+
+export function StandupReport({today,yesterday,blockers, name, icon}:StandupProps) {
+    return (
+        <Box elevation="small" pad="medium" basis="medium">
+        <Box align="center">
+            <Box width="120px" align="center" alignContent="center" height="120px" round="full" overflow="hidden" border="all">
+                {icon}
+            </Box>
+            <Heading size="24px">{name}</Heading>
+
+            <Text weight="bold">
+                Plan for today
+            </Text>
+            <Text margin="small">
+            {today}
+            </Text>
+
+            <Text weight="bold">
+                Accomplished Yesterday
+            </Text>
+            <Text margin="small">
+                {yesterday}
+            </Text>
+
+            <Text weight="bold">
+                Blockers
+            </Text>
+            <Text margin="small">
+                {blockers ? blockers : "None"}
+            </Text>
+        </Box>
+    </Box>
+    )
+}
--- demo/src/pages/home.tsx
+++ demo/src/pages/home.tsx
@@ -1,10 +1,11 @@
-import React, { useState, FormEvent } from 'react';
+import React, { useState } from 'react';
 import { Box, Heading, Image, Text, Form, FormField, TextArea } from 'grommet';
 import { useAmbientUser } from '../util/user';
 import { useDecentralizedDatabase } from '../util/usedatabase';
 
 import woman from '../images/woman.jpg'
-import { Previous } from 'grommet-icons';
+import { Previous, UserFemale, User, Next } from 'grommet-icons';
+import { StandupReport } from '../components/standupreport';
 
 interface message {
     body: string
@@ -51,10 +52,10 @@ export function Home() {
     return (
         <Box fill align="center" justify="center">
             <Box fill pad="small">
-                <Heading size="medium">Async daily standups</Heading>
+                <Heading size="medium">Daily Standups</Heading>
                 <Box background="light-2" alignContent="center" justify="around" direction="row" color="light" pad="medium">
                     <Box basis="small" alignSelf="center">
-                        <Previous size="xlarge"/>
+                        <Previous size="xlarge" />
                     </Box>
                     <Box elevation="small" pad="medium" basis="1/4">
                         <Heading size="small">Yesterday</Heading>
@@ -75,14 +76,54 @@ export function Home() {
                             </Form>
                         </Box>
                     </Box>
-                    <Box pad="medium"  align="center" alignSelf="center" basis="small">
+                    <Box pad="medium" align="center" alignSelf="center" basis="small">
                         <Box width="120px" height="120px" round="full" overflow="hidden">
                             <Image fit="cover" src={woman} />
                         </Box>
                         <Text>You</Text>
-
                     </Box>
                 </Box>
+
+                <Box align="center">
+                    <Heading size="small">Today's Team Updates</Heading>
+                </Box>
+
+                <Box alignContent="center" justify="around" direction="row" pad="medium">
+
+                    <Box basis="small" alignSelf="center">
+                        <Previous size="medium" />
+                    </Box>
+
+                    <StandupReport 
+                        name="Alice"
+                        icon={<UserFemale size="xlarge"/>}
+                        today={`Beard copper mug biodiesel, chillwave pork belly quinoa +1. Enamel pin vinyl sriracha forage. Tbh mumblecore cronut yr skateboard. Hot chicken pickled ugh tousled gluten-free.`}
+                        yesterday={`Tattooed raclette chicharrones occupy enamel pin coloring book neutra etsy disrupt woke copper mug portland. Slow-carb squid enamel pin, four loko 8-bit intelligentsia small batch keytar shabby chic fingerstache jean short`}
+                    />
+
+                    <StandupReport 
+                        name="Bob"
+                        icon={<User size="xlarge"/>}
+                        today={`Beard copper mug biodiesel, chillwave pork belly quinoa +1. Enamel pin vinyl sriracha forage. Tbh mumblecore cronut yr skateboard. Hot chicken pickled ugh tousled gluten-free.`}
+                        yesterday={`Tattooed raclette chicharrones occupy enamel pin coloring book neutra etsy disrupt woke copper mug portland. Slow-carb squid enamel pin, four loko 8-bit intelligentsia small batch keytar shabby chic fingerstache jean short`}
+                    />
+
+                    <StandupReport 
+                        name="Carol"
+                        icon={<UserFemale size="xlarge"/>}
+                        today={`Beard copper mug biodiesel, chillwave pork belly quinoa +1. Enamel pin vinyl sriracha forage. Tbh mumblecore cronut yr skateboard. Hot chicken pickled ugh tousled gluten-free.`}
+                        yesterday={`Tattooed raclette chicharrones occupy enamel pin coloring book neutra etsy disrupt woke copper mug portland. Slow-carb squid enamel pin, four loko 8-bit intelligentsia small batch keytar shabby chic fingerstache jean short`}
+                    />
+
+                    
+
+                    <Box basis="small" alignSelf="center">
+                        <Next size="medium" />
+                    </Box>
+                </Box>
+
+                    
+
             </Box>
         </Box>
     )
